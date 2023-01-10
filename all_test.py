01a820f9f1ca82e602124a90e477eb17d8aa7192
Massimiliano Mola <massimiliano.mola.bzs@gmail.com>
2023-01-10 23:04:42 +0100

    orderd test file

--- all_test.py
+++ all_test.py
@@ -3,14 +3,19 @@ from src.notifyer.main import *
 import os
 
 class Test(unittest.TestCase):
+    # Variables for testing
     students = import_students(os.environ.get('PATH_MENSA'))
+    students_today = get_students_today(import_students(os.environ.get('PATH_MENSA')))
+    # ---------------------------------------------------------------- 
+    # Global
+    # ----------------------------------------------------------------
+    def test_env_variables(self):
+        self.assertEqual(os.environ.get('PATH_MENSA'), "C:/Users/massi/Documents/School/5IA/projekt_matura/data/Mensa classi prime.csv")
 
-    def test_get_students_today(self):
-        try:
-            get_students_today(self.students)
-        except Exception:
-            self.fail("Unexpected exception")
 
+    # ---------------------------------------------------------------- 
+    # Notyfier
+    # ----------------------------------------------------------------
     def test_get_absences(self):
         try:
             get_absences(self.students)
@@ -18,13 +23,34 @@ class Test(unittest.TestCase):
             self.fail("Unexpected exception")
         pass
 
-    def test_env_variables(self):
-        self.assertEqual(os.environ.get('PATH_MENSA'), "C:/Users/massi/Documents/School/5IA/projekt_matura/data/Mensa classi prime.csv")
 
     def test_get_mensa_list(self):
         x = get_mensa_list()
         self.assertTrue(isinstance(x, list)) 
+
+
+    def test_check_absences(self):
+        ab = get_absences(self.students_today)
+        pr = get_presences(self.students_today)
+
+        stu =  ab + pr
+
+        for s in self.students_today:
+            if not s in stu:
+                self.fail("not all elements presett in array")
+
+    # ---------------------------------------------------------------- 
+    # Student
+    # ----------------------------------------------------------------
+    def test_get_students_today(self):
+        try:
+            get_students_today(self.students)
+        except Exception:
+            self.fail("Unexpected exception")
+
+
         
+
 
 if __name__ == '__main__':
     unittest.main()
